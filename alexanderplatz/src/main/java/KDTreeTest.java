a73256998603ae31d816ac36e02b337336a51e02
Tilmann Schlenther <schlenther@vsp.tu-berlin.de>
2017-06-20 12:28:20 +0200

    habe einen JUnit Test hinfzugefügt, library eingebunden

---
+++ alexanderplatz/src/main/java/KDTreeTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author tilmann
+ *
+ */
+public class KDTreeTest {
+	
+	
+	
+	@Test
+	public void testKDTree(){
+		
+		Vehicle v1 = new Vehicle();
+		
+		
+	}
+}
