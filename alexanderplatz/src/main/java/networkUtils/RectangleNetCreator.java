30214f08ff58fd0e4dd6dee2bca18504b9d83ae6
Tilmann Schlenther <schlenther@vsp.tu-berlin.de>
2017-07-06 11:09:56 +0200

    eine kleine Hilfklasse zum Erstellen eines simplen Netzwerkes in Form eines Rechtecks

---
+++ alexanderplatz/src/main/java/networkUtils/RectangleNetCreator.java
@@ -0,0 +1,30 @@
+package networkUtils;
+
+public class RectangleNetCreator {
+
+	private Network network;
+	
+	public RectangleNetCreator(){
+		this.network = new Network();
+	}
+	
+	public Network createNetwork(){
+		Node n0 = this.network.createNode(0, 0, 1);
+		Node n1 = this.network.createNode(10,0,2);
+		Node n2 = this.network.createNode(10, 10, 3);
+		Node n3 = this.network.createNode(0, 10, 4);
+		
+		this.network.createLink(n0, n1, 0);
+		this.network.createLink(n1, n0, 0);
+		this.network.createLink(n0, n3, 0);
+		this.network.createLink(n3, n0, 0);
+		this.network.createLink(n1, n2, 0);
+		this.network.createLink(n2, n1, 0);
+		this.network.createLink(n2, n3, 0);
+		this.network.createLink(n3, n2, 0);
+	
+		
+		return this.network;
+	}
+	
+}
