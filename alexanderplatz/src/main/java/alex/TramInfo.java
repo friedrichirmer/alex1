65e2d8eb6d7989021ca13bcad08cb59d1947cdc6
Tilmann Schlenther <schlenther@vsp.tu-berlin.de>
2017-07-17 18:06:29 +0200

    tram rotiert noch nicht... und crash check funktioniert gerade auch nicht

--- alexanderplatz/src/main/java/alex/TramInfo.java
+++ alexanderplatz/src/main/java/alex/TramInfo.java
@@ -20,7 +20,8 @@ public class TramInfo {
 	
 	private float xCenter;
 	private float yCenter;
-	
+	private float xBottom;
+	private float yBottom;
     
 	public TramInfo(Tram tram) {
 		this.walls = tram.getWalls();
@@ -30,19 +31,24 @@ public class TramInfo {
 		this.length = tram.getLength();
 		this.xCenter = (float) tram.getX();
 		this.yCenter = (float) tram.getY();
+		this.xBottom = tram.getBottomX();
+		this.yBottom = tram.getBottomY();
 	}
 
 	public void draw(PApplet p){
 		p.strokeWeight(2);
-		p.fill(100, 0 , 0);
+		p.stroke(255,0,0);
 //		p.rect((float)left.getX1(), (float)left.getY1(), width, length);
-		p.fill(0,200,0);
-		p.ellipse(xCenter, yCenter, 5, 5);
+		p.fill(255,0,0);
+//		p.ellipse(xCenter * Vis.scale, yCenter* Vis.scale, 5, 5);
+		p.ellipse(xBottom * Vis.scale, yBottom * Vis.scale, 5, 5);
 		drawWalls(p);
 	}
 	
 	private void drawWalls(PApplet p) {
 		Iterator<Wall> it = walls.iterator();
+		p.strokeWeight(1);
+		p.stroke(0);
 		while(it.hasNext()){
 			Wall wa = it.next();
    	    	float wx1 = (float)(wa.getX1()* Vis.scale) ;
